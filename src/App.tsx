220aa83b330acc8945df22bcba4730f16d3c15fb
AyubBodirkhonov <apple@Ayubs-MacBookAir.local>
2022-05-22 20:26:46 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import AddToList from './components/AddToList';
+import List from './components/List'
 
+export interface IState {
+  people: {
+    name: string,
+    age: number,
+    url: string,
+    note?: string
+  }[]
+}
 function App() {
+
+  const [people, setPeople] = useState<IState['people']>([
+    {
+      name: "LeBron James",
+      url: "https://phantom-marca.unidadeditorial.es/81bcb0e771d00460643883701caab97d/resize/1320/f/jpg/assets/multimedia/imagenes/2022/05/20/16530820031752.jpg",
+      age: 36,
+      note: "The best player of the Year"
+    }
+  ])
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>People Invited to my Party</h1>
+        <List people={people}/>
+        <AddToList people={people} setPeople={setPeople}/>
     </div>
   );
 }
